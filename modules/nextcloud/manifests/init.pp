a4384836f83321500080ce8707e72eb490baecf7
batcsg1 <batcsg1@student.op.ac.nz>
2026-08-23 15:52:48 +1200

    chore: updated nextcloud module to use common and secret variables

--- modules/nextcloud/manifests/init.pp
+++ modules/nextcloud/manifests/init.pp
@@ -1,9 +1,9 @@
 class nextcloud (
-  String  $compose_dir       = '/opt/docker/nextcloud',
-  String  $nextcloud_home    = '/srv/nextcloud',
-  String  $mysql_password    = 'JoinKnight67^&',
-  String  $domain_name       = 'cloud.batchelornz.com',
-  Optional[String] $cloudflare_tunnel_token = 'eyJhIjoiOGY5N2M3ZDEzYmI5ZGQ0YzQ0MWEyMTJhMWM2NTMxZDUiLCJ0IjoiOWQ0ZDMxYzktMzg5ZS00OWIxLWI1MTktYzg2MzYxNWM1ODRiIiwicyI6Ik5UY3haREpoTnpRdFltTmlOUzAwTnpGbExXSXpZMlV0TkRVNE0ySmtObUkyWTJSbCJ9',
+  String  $compose_dir,
+  String  $nextcloud_home,
+  String  $mysql_password,
+  String  $domain_name,
+  Optional[String] $cloudflare_tunnel_token,
 ) {
   contain nextcloud::install
   contain nextcloud::config
