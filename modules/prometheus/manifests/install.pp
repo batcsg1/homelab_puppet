3b8d40b9802ffdb5a9645f183b5df18644d30246
batcsg1 <batcsg1@student.op.ac.nz>
2026-09-04 21:23:33 +1200

    fix(prometheus): added require to both file trees

--- modules/prometheus/manifests/install.pp
+++ modules/prometheus/manifests/install.pp
@@ -1,67 +1,68 @@
 class prometheus::install {
 
-  # Download Prometheus 2.53.4
-  exec { 'download_prometheus':
-    command => 'wget https://github.com/prometheus/prometheus/releases/download/v2.53.4/prometheus-2.53.4.linux-amd64.tar.gz',
-    cwd     => '/opt',
-    creates => '/opt/prometheus-2.53.4.linux-amd64.tar.gz',
-    path    => ['/usr/bin', '/bin'],
-  }
+  $version = '2.53.4'
+  $install_dir = '/usr/local/bin/prometheus'
 
-  # Extract the tarball
-  exec { 'extract_prometheus':
-    command => 'tar -xvf prometheus-2.53.4.linux-amd64.tar.gz',
-    cwd     => '/opt',
-    creates => '/opt/prometheus-2.53.4.linux-amd64/prometheus',
-    path    => ['/usr/bin', '/bin'],
-    require => Exec['download_prometheus'],
-  }
-
-  # Create the Prometheus installation directory
-  file { '/usr/local/bin/prometheus':
-    ensure => directory,
-    owner  => 'prometheus',
-    group  => 'prometheus',
-    mode   => '0755',
-  }
-
-  # Copy the entire extracted directory contents
-  exec { 'copy_prometheus_tree':
-    command => 'cp -r /opt/prometheus-2.53.4.linux-amd64/* /usr/local/bin/prometheus/',
-    creates => '/usr/local/bin/prometheus/prometheus',
-    path    => ['/usr/bin', '/bin'],
-    require => Exec['extract_prometheus'],
-  }
-
-  # Create the Prometheus user (no home, no shell)
   user { 'prometheus':
     ensure     => present,
     managehome => false,
     shell      => '/usr/sbin/nologin',
+    system     => true,
   }
 
-  # Parent directory MUST exist first
+  exec { 'download_prometheus':
+    command => "wget https://github.com/prometheus/prometheus/releases/download/v${version}/prometheus-${version}.linux-amd64.tar.gz",
+    cwd     => '/opt',
+    creates => "/opt/prometheus-${version}.linux-amd64.tar.gz",
+    path    => ['/usr/bin', '/bin'],
+  }
+
+  exec { 'extract_prometheus':
+    command => "tar -xvf prometheus-${version}.linux-amd64.tar.gz",
+    cwd     => '/opt',
+    creates => "/opt/prometheus-${version}.linux-amd64/prometheus",
+    path    => ['/usr/bin', '/bin'],
+    require => Exec['download_prometheus'],
+  }
+
+  file { $install_dir:
+    ensure  => directory,
+    owner   => 'prometheus',
+    group   => 'prometheus',
+    mode    => '0755',
+    require => User['prometheus'],
+  }
+
+  exec { 'copy_prometheus_tree':
+    command  => "cp -r /opt/prometheus-${version}.linux-amd64/* ${install_dir}/",
+    creates  => "${install_dir}/prometheus",
+    path     => ['/usr/bin', '/bin'],
+    provider => shell,
+    require  => [Exec['extract_prometheus'], File[$install_dir]],
+  }
+
   file { '/var/lib/prometheus':
-    ensure => directory,
-    owner  => 'prometheus',
-    group  => 'prometheus',
-    mode   => '0755',
+    ensure  => directory,
+    owner   => 'prometheus',
+    group   => 'prometheus',
+    mode    => '0755',
+    require => User['prometheus'],
   }
 
-  # Create the data directory
   file { '/var/lib/prometheus/data':
-    ensure => directory,
-    owner  => 'prometheus',
-    group  => 'prometheus',
-    mode   => '0755',
+    ensure  => directory,
+    owner   => 'prometheus',
+    group   => 'prometheus',
+    mode    => '0755',
     require => File['/var/lib/prometheus'],
   }
 
-  # Ensure ownership of the Prometheus binary tree
   exec { 'fix_prometheus_permissions':
-    command => 'chown -R prometheus:prometheus /usr/local/bin/prometheus',
-    path    => ['/usr/bin', '/bin'],
-    require => [User['prometheus'], Exec['copy_prometheus_tree']],
+    command     => "chown -R prometheus:prometheus ${install_dir}",
+    path        => ['/usr/bin', '/bin'],
+    refreshonly => true,
+    subscribe   => Exec['copy_prometheus_tree'],
+    require     => User['prometheus'],
   }
 
 }
