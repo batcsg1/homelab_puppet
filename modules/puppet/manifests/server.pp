d05507efa4a582323713e3ba76d3e723d1ae065d
batcsg1 <batcsg1@student.op.ac.nz>
2026-09-06 21:28:13 +1200

    fix: deleted ca_line variable

--- modules/puppet/manifests/server.pp
+++ modules/puppet/manifests/server.pp
@@ -1,24 +1,19 @@
 class puppet::server (
-  String $certname,
-  String $server,
-  String $basemodulepath,
-  String $runinterval,
-  String $dns_alt_names,
-  String $vardir,
-  String $logdir,
-  String $rundir,
-  String $codedir,
-  Boolean $ca_master = true,
+  String  $certname,
+  String  $server,
+  String  $basemodulepath,
+  String  $runinterval,
+  String  $dns_alt_names,
+  String  $vardir,
+  String  $logdir,
+  String  $rundir,
+  String  $codedir,
+  Boolean $ca_master = false,
   String  $ca_server = $server,
 ) {
   # true  => this host runs the CA (CA master)
   # false => CA service disabled, certs come from $ca_server (compile master)
   $ca_service = $ca_master ? {
-    true    => 'certificate-authority-service',
-    default => 'certificate-authority-disabled-service',
-  }
-
-  $ca_line = $ca_service ? {
     true    => 'certificate-authority-service',
     default => 'certificate-authority-disabled-service',
   }
@@ -45,16 +40,16 @@ class puppet::server (
 
   file { '/etc/puppet/puppetserver/services.d':
     ensure => directory,
-    owner  => 'puppet',
-    group  => 'puppet',
-    mode   => '0755', 
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
   }
 
   # CA enabled/disabled is a Puppetserver bootstrap toggle, not a puppet.conf setting.
   file { '/etc/puppet/puppetserver/services.d/ca.cfg':
     ensure  => file,
     content => @("CACFG"),
-      puppetlabs.services.ca.${ca_line}/${ca_line}
+      puppetlabs.services.ca.${ca_service}/${ca_service}
       puppetlabs.trapperkeeper.services.watcher.filesystem-watch-service/filesystem-watch-service
       | CACFG
     owner   => 'root',
